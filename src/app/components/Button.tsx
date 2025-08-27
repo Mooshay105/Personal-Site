01df269a5457bbe7b19285a8b4b3193dbc97ecba
Malcolm Hauser <hauser.malcolm@gmail.com>
2025-08-27 12:07:59 +1000

    cleanup

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -15,32 +15,21 @@ function Button({
 	externalLink = false,
 	extraClass = "",
 }: ButtonProps) {
+	const css =
+		(isCallToAction
+			? "bg-[radial-gradient(circle_at_0_0,_var(--primary-color),_var(--black)_42%)] m-[10px] p-[10px_20px] rounded-[10px] shadow-[var(--box-shadow-primary-rest)] border-[1px] border-solid border-[var(--primary-color)] transition-all duration-200 ease-in-out hover:shadow-[var(--box-shadow-primary-hover)] hover:translate-x-[1px] hover:translate-y-[1px] active:shadow-none active:translate-x-[3px] active:translate-y-[3px] "
+			: "m-[10px] p-[10px_20px] rounded-[10px] shadow-[var(--box-shadow-secondary-rest)] border-[1px] border-solid border-[var(--secondary-color)] transition-all duration-200 ease-in-out hover:shadow-[var(--box-shadow-secondary-hover)] hover:translate-x-[1px] hover:translate-y-[1px] active:shadow-none active:translate-x-[3px] active:translate-y-[3px] ") +
+		extraClass;
+
 	if (externalLink) {
 		return (
-			<Link
-				className={
-					(isCallToAction
-						? "bg-[radial-gradient(circle_at_0_0,_var(--primary-color),_var(--black)_42%)] m-[10px] p-[10px_20px] rounded-[10px] shadow-[var(--box-shadow-primary-rest)] border-[1px] border-solid border-[var(--primary-color)] transition-all duration-200 ease-in-out hover:shadow-[var(--box-shadow-primary-hover)] hover:translate-x-[1px] hover:translate-y-[1px] active:shadow-none active:translate-x-[3px] active:translate-y-[3px] "
-						: "m-[10px] p-[10px_20px] rounded-[10px] shadow-[var(--box-shadow-secondary-rest)] border-[1px] border-solid border-[var(--secondary-color)] transition-all duration-200 ease-in-out hover:shadow-[var(--box-shadow-secondary-hover)] hover:translate-x-[1px] hover:translate-y-[1px] active:shadow-none active:translate-x-[3px] active:translate-y-[3px] ") +
-					extraClass
-				}
-				href={link}
-				target="_blank"
-			>
+			<Link className={css} href={link} target="_blank">
 				{text}
 			</Link>
 		);
 	}
 	return (
-		<Link
-			className={
-				(isCallToAction
-					? "bg-[radial-gradient(circle_at_0_0,_var(--primary-color),_var(--black)_42%)] m-[10px] p-[10px_20px] rounded-[10px] shadow-[var(--box-shadow-primary-rest)] border-[1px] border-solid border-[var(--primary-color)] transition-all duration-200 ease-in-out hover:shadow-[var(--box-shadow-primary-hover)] hover:translate-x-[1px] hover:translate-y-[1px] active:shadow-none active:translate-x-[3px] active:translate-y-[3px] "
-					: "m-[10px] p-[10px_20px] rounded-[10px] shadow-[var(--box-shadow-secondary-rest)] border-[1px] border-solid border-[var(--secondary-color)] transition-all duration-200 ease-in-out hover:shadow-[var(--box-shadow-secondary-hover)] hover:translate-x-[1px] hover:translate-y-[1px] active:shadow-none active:translate-x-[3px] active:translate-y-[3px] ") +
-				extraClass
-			}
-			href={link}
-		>
+		<Link className={css} href={link}>
 			{text}
 		</Link>
 	);
